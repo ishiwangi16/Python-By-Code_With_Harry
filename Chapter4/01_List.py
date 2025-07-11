fe98d1cde40a3ea1e3cadff384a4b1c4756d9f37
ishiwangi16 <2022ugee009@nitjsr.ac.in>
2025-07-12 01:04:28 +0530

    Add solution for Meeting Rooms III problem

---
+++ Chapter4/01_List.py
@@ -0,0 +1,10 @@
+friends=["Apple","Orange","Shiwangi"]
+print(friends[2])
+
+friends[2]="Grapes"
+
+print(friends[2])
+
+# We can change the lists but not in th strings
+print(friends[1:3])
+
---
+++ Chapter4/list_Method.py
@@ -0,0 +1,21 @@
+friends=["Apple","Orange","Shiwangi"]
+friends.append("Harry")
+# Append in the last
+print(friends)
+
+
+ll=[15,78,74]
+ll.sort()
+print(ll)
+
+ll.reverse()
+print(ll)
+
+ll.insert(2,3333)
+print(ll)
+
+ll.pop(2)
+print(ll)
+
+ll.remove(15)
+print(ll)
